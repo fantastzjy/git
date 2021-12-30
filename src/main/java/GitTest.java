ab2c867216cf8cba79c78962f35a273d77dae04a
fantastzjy <849372293@qq.com>
2021-12-30 17:19:46 +0800

    Initial commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,165 @@
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.RefSpec;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.junit.Test;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Package: PACKAGE_NAME
+ * @ClassName: gitTest
+ * @Author: jiaying2.zhang
+ * @CreateTime: 2021-12-29 10:56
+ * @Description:
+ */
+public class GitTest {
+
+    public static void main(String[] args) throws GitAPIException, IOException {
+
+        String remoteRepositoryUrl = "https://github.com/fantastzjy/jgit";
+        String cloneToLocalDir = "jgit";
+        String localRepositoryPath = "D:/MyConfiguration/jiaying2.zhang/Desktop/git";
+
+        String username = "fantastzjy";
+        String password = "Fantastu7.";
+
+        // 凭证管理
+        CredentialsProvider provider = createCredential(username, password);
+
+        // clone远程仓库
+//        Git git = CloneRepositoryFromRemote(remoteRepositoryUrl, cloneToLocalDir, provider);
+
+        //获取本地仓库
+        Repository repositoryFromDir = getRepositoryFromDir(localRepositoryPath);
+        repositoryFromDir.getDirectory();
+        String branch = repositoryFromDir.getBranch();
+
+
+        //commit
+        // Git git, String message, CredentialsProvider provider
+//        commit(git, "测试提交", provider);
+//        push(git, provider);
+
+        System.out.println("结束");
+    }
+
+
+    //    To build a repository
+//    @Test
+//    public static Repository repositoryBuild(String pathName) {
+//        try {
+//            FileRepositoryBuilder builder = new FileRepositoryBuilder();
+//            Repository repository = builder.setGitDir(new File(pathName))
+//                    .readEnvironment() // scan environment GIT_* variables
+//                    .findGitDir() // scan up the file system tree
+//                    .build();
+//            return repository;
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+
+
+    //凭证
+    // 通过CredentialsProvider管理凭证，常用的是UsernamePasswordCredentialsProvider
+    public static CredentialsProvider createCredential(String userName, String password) {
+        return new UsernamePasswordCredentialsProvider(userName, password);
+    }
+
+    //clone远程仓库
+    //通过Git.cloneRepository -> clone远程仓库，如果需要凭证，则需要指定credentialsProvider
+    public static Git CloneRepositoryFromRemote(String repoUrl, String cloneDir, CredentialsProvider provider) throws GitAPIException {
+        Git git = Git.cloneRepository()
+                .setCredentialsProvider(provider)
+                .setURI(repoUrl)
+                .setDirectory(new File(cloneDir)).call();
+        return git;
+    }
+
+    //若已克隆，读取已有仓库
+    public static Repository getRepositoryFromDir(String dir) throws IOException {
+        return new FileRepositoryBuilder()
+                .setGitDir(Paths.get(dir, ".git").toFile())
+                .build();
+    }
+
+    //    commit
+    //    git 命令： git commit -a -m '{msg}'
+    //    commit前先add
+    public static void commit(Git git, String message, CredentialsProvider provider) throws GitAPIException {
+        // 添加文件
+        git.add().addFilepattern(".").call();
+        //提交
+        git.commit()
+                .setMessage(message)
+                .call();
+
+    }
+
+    //    push
+    //    git 命令： git push origin branchName
+    //    push直接调用push即可,需要指定credentialsProvider
+    public static void push(Git git, CredentialsProvider provider) throws GitAPIException, IOException {
+        push(git, null, provider);
+    }
+
+    public static void push(Git git, String branch, CredentialsProvider provider) throws GitAPIException, IOException {
+        if (branch == null) {
+            branch = git.getRepository().getBranch();
+        }
+        git.push()
+                .setCredentialsProvider(provider)
+                .setRemote("origin").setRefSpecs(new RefSpec(branch)).call();
+    }
+
+
+    //    读取仓库日志
+    //    可以通过RevWalk读取仓库日志。
+    //    revWalk.parseCommit 可读取一条commit
+    //    遍历revWalk，可读取所有日志
+    public static List<String> getLogs(Repository repository) throws IOException {
+        return getLogsSinceCommit(repository, null, null);
+    }
+
+    public static List<String> getLogsSinceCommit(Repository repository, String commit) throws IOException {
+        return getLogsSinceCommit(repository, null, commit);
+    }
+
+    public static List<String> getLogsSinceCommit(Repository repository, String branch, String commit) throws IOException {
+        if (branch == null) {
+            branch = repository.getBranch();
+        }
+        Ref head = repository.findRef("refs/heads/" + branch);
+        List<String> commits = new ArrayList<>();
+        if (head != null) {
+            try (RevWalk revWalk = new RevWalk(repository)) {
+                revWalk.markStart(revWalk.parseCommit(head.getObjectId()));
+                for (RevCommit revCommit : revWalk) {
+                    if (revCommit.getId().getName().equals(commit)) {
+                        break;
+                    }
+                    commits.add(revCommit.getFullMessage());
+                    System.out.println("\n Commit-Message: " + revCommit.getFullMessage());
+                }
+                revWalk.dispose();
+            }
+        }
+
+        return commits;
+    }
+
+}
---
+++ src/main/java/pojo/GitProject.java
@@ -0,0 +1,173 @@
+package pojo;
+
+import java.util.*;
+
+/**
+ * @Package: pojo
+ * @ClassName: GitProject
+ * @Author: jiaying2.zhang
+ * @CreateTime: 2021-12-30 14:22
+ * @Description:
+ */
+public class GitProject {
+
+
+    /**
+     * 项目名
+     */
+    private String name;
+
+    /**
+     * 远程地址
+     */
+    private String remoteUrl;
+
+    /**
+     * 最后提交id
+     */
+    private String lastCommitId;
+
+    /**
+     * 最后提交日志
+     */
+    private String lastCommitMessage;
+
+    /**
+     * 最后提交人
+     */
+    private String lastCommitUser;
+
+    /**
+     * 最后提交邮箱
+     */
+    private String lastCommitEmail;
+
+    /**
+     * 最后提交时间
+     */
+    private Date lastCommitDate;
+
+    /**
+     * 当前分支
+     */
+    private String currBranch;
+
+    /**
+     * 分支集合
+     */
+    private final List<String> branchList = new ArrayList<>();
+
+    /**
+     * 标签集合
+     */
+    private final List<String> tagList = new ArrayList<>();
+
+    /**
+     * 仓库文件新增集合
+     */
+    private final Set<String> untrackedSet = new HashSet<>();
+
+    /**
+     * 仓库文件修改集合
+     */
+    private final Set<String> modifiedSet = new HashSet<>();
+
+    /**
+     * 仓库文件删除集合
+     */
+    private final Set<String> missingSet = new HashSet<>();
+
+    /**
+     * 仓库文件冲突集合
+     */
+    private final Set<String> conflictingSet = new HashSet<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRemoteUrl() {
+        return remoteUrl;
+    }
+
+    public void setRemoteUrl(String remoteUrl) {
+        this.remoteUrl = remoteUrl;
+    }
+
+    public String getLastCommitId() {
+        return lastCommitId;
+    }
+
+    public void setLastCommitId(String lastCommitId) {
+        this.lastCommitId = lastCommitId;
+    }
+
+    public String getLastCommitMessage() {
+        return lastCommitMessage;
+    }
+
+    public void setLastCommitMessage(String lastCommitMessage) {
+        this.lastCommitMessage = lastCommitMessage;
+    }
+
+    public String getLastCommitUser() {
+        return lastCommitUser;
+    }
+
+    public void setLastCommitUser(String lastCommitUser) {
+        this.lastCommitUser = lastCommitUser;
+    }
+
+    public String getLastCommitEmail() {
+        return lastCommitEmail;
+    }
+
+    public void setLastCommitEmail(String lastCommitEmail) {
+        this.lastCommitEmail = lastCommitEmail;
+    }
+
+    public String getCurrBranch() {
+        return currBranch;
+    }
+
+    public void setCurrBranch(String currBranch) {
+        this.currBranch = currBranch;
+    }
+
+    public Date getLastCommitDate() {
+        return lastCommitDate;
+    }
+
+    public void setLastCommitDate(Date lastCommitDate) {
+        this.lastCommitDate = lastCommitDate;
+    }
+
+    public List<String> getBranchList() {
+        return branchList;
+    }
+
+    public List<String> getTagList() {
+        return tagList;
+    }
+
+    public Set<String> getUntrackedSet() {
+        return untrackedSet;
+    }
+
+    public Set<String> getModifiedSet() {
+        return modifiedSet;
+    }
+
+    public Set<String> getMissingSet() {
+        return missingSet;
+    }
+
+    public Set<String> getConflictingSet() {
+        return conflictingSet;
+    }
+
+}
