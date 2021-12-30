e36c9a8d1ffcfb4e0958ab959366782b1f16102e
fantastzjy <849372293@qq.com>
2021-12-30 19:53:57 +0800

    测试提交

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,7 +1,11 @@
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ResetCommand;
+import org.eclipse.jgit.api.Status;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -10,7 +14,6 @@ import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.transport.CredentialsProvider;
 import org.eclipse.jgit.transport.RefSpec;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
-import org.junit.Test;
 
 
 import java.io.File;
@@ -26,53 +29,70 @@ import java.util.List;
  * @CreateTime: 2021-12-29 10:56
  * @Description:
  */
+@Slf4j
 public class GitTest {
+
+    //仓库路径
+    public static String remoteRepositoryUrl = "https://github.com/fantastzjy/jgit";
+    public static String cloneToLocalDir = "jgit";
+    public static String localRepositoryPath = "D:/MyConfiguration/jiaying2.zhang/Desktop/git";
+    public static String buildPathName = "D:/MyConfiguration/jiaying2.zhang/Desktop/ggg";
+    //账户
+    public static String username = "fantastzjy";
+    public static String password = "Fantastu7.";
+    // 凭证管理
+    public static CredentialsProvider provider = createCredential(username, password);
+
 
     public static void main(String[] args) throws GitAPIException, IOException {
 
-        String remoteRepositoryUrl = "https://github.com/fantastzjy/jgit";
-        String cloneToLocalDir = "jgit";
-        String localRepositoryPath = "D:/MyConfiguration/jiaying2.zhang/Desktop/git";
-        String buildPathName = "D:/MyConfiguration/jiaying2.zhang/Desktop/ggg";
-
-        String username = "fantastzjy";
-        String password = "Fantastu7.";
-
-        // 凭证管理
-        CredentialsProvider provider = createCredential(username, password);
-
-        //1、仓库相关
-
-        // clone远程仓库
+        //************ 仓库相关
+        //1、 clone远程仓库
 //        Git git = CloneRepositoryFromRemote(remoteRepositoryUrl, cloneToLocalDir, provider);
 
-//        构建本地仓库
+        //2、获取本地仓库
+        //方式一
+//        Repository repositoryFromDir = getRepositoryFromDir(localRepositoryPath);
+//        Git git = new Git(repositoryFromDir);
+        //方式二
+        Git git = Git.open(new File(localRepositoryPath));
+
+        //3、构建本地仓库
 //        Repository repository = repositoryBuild(buildPathName);
 
-        //获取本地仓库
-        //方式一
-        Repository repositoryFromDir = getRepositoryFromDir(localRepositoryPath);
-        Git git = new Git(repositoryFromDir);
-        //方式二
-//        Git git = Git.open(new File(localRepositoryPath));   //√
 
-        //2、commit   √
+        //************* commit
         commit(git, "测试提交", provider);
 
-        //3、push
+        //************* push
 //        push(git, provider);
 
-        System.out.println("结束");
+        //************* pull  git为本地仓库的Git
+        Git pull = pull(git, provider);
+
+        System.out.println("结束..........");
     }
 
-    //Http凭证
-    //通过CredentialsProvider管理凭证，常用的是UsernamePasswordCredentialsProvider
+    /**
+     * Http凭证
+     * 通过CredentialsProvider管理凭证，常用的是UsernamePasswordCredentialsProvider
+     *
+     * @param userName
+     * @param password
+     * @return
+     */
     public static CredentialsProvider createCredential(String userName, String password) {
         return new UsernamePasswordCredentialsProvider(userName, password);
     }
 
-    //仓库相关
-    //构建仓库
+    //*********仓库相关
+
+    /**
+     * 构建仓库
+     *
+     * @param buildPathName
+     * @return
+     */
     public static Repository repositoryBuild(String buildPathName) {
         try {
             FileRepositoryBuilder builder = new FileRepositoryBuilder();
@@ -87,8 +107,16 @@ public class GitTest {
         return null;
     }
 
-    //clone远程仓库
-    //通过Git.cloneRepository -> clone远程仓库，如果需要凭证，则需要指定credentialsProvider
+    /**
+     * clone远程仓库
+     * 通过Git.cloneRepository -> clone远程仓库，如果需要凭证，则需要指定credentialsProvider
+     *
+     * @param repoUrl
+     * @param cloneDir
+     * @param provider
+     * @return
+     * @throws GitAPIException
+     */
     public static Git CloneRepositoryFromRemote(String repoUrl, String cloneDir, CredentialsProvider provider) throws GitAPIException {
         //CloneCommand cloneCommand = Git.cloneRepository();
         //对cloneCommand再次进行了封装
@@ -99,16 +127,29 @@ public class GitTest {
         return git;
     }
 
-    //若已克隆，读取已有仓库
+    /**
+     * 若已克隆，读取已有仓库
+     *
+     * @param dir
+     * @return
+     * @throws IOException
+     */
     public static Repository getRepositoryFromDir(String dir) throws IOException {
         return new FileRepositoryBuilder()
                 .setGitDir(Paths.get(dir, ".git").toFile())
                 .build();
     }
 
-    //    commit
-    //    git 命令： git commit -a -m '{msg}'
-    //    commit前先add
+    /**
+     * commit
+     * git 命令： git commit -a -m '{msg}'
+     * commit前先add
+     *
+     * @param git
+     * @param message
+     * @param provider
+     * @throws GitAPIException
+     */
     public static void commit(Git git, String message, CredentialsProvider provider) throws GitAPIException {
         // 添加文件
         git.add().addFilepattern(".").call();
@@ -119,12 +160,28 @@ public class GitTest {
 
     }
 
-    //    push
-    //    git 命令： git push origin branchName    push直接调用push即可,需要指定credentialsProvider
+    /**
+     * push
+     * git 命令： git push origin branchName    push直接调用push即可,需要指定credentialsProvider
+     *
+     * @param git
+     * @param provider
+     * @throws GitAPIException
+     * @throws IOException
+     */
     public static void push(Git git, CredentialsProvider provider) throws GitAPIException, IOException {
         push(git, null, provider);
     }
 
+    /**
+     * 自动获取分支名称
+     *
+     * @param git
+     * @param branch
+     * @param provider
+     * @throws GitAPIException
+     * @throws IOException
+     */
     public static void push(Git git, String branch, CredentialsProvider provider) throws GitAPIException, IOException {
         if (branch == null) {
             branch = git.getRepository().getBranch();
@@ -132,6 +189,66 @@ public class GitTest {
         git.push()
                 .setCredentialsProvider(provider)
                 .setRemote("origin").setRefSpecs(new RefSpec(branch)).call();
+    }
+
+
+    /**
+     * pull 拉取
+     */
+    public static Git pull(Git git,  CredentialsProvider provider) throws IOException {
+        return pull(git, null, provider);
+    }
+    public static Git pull(Git git, String branch, CredentialsProvider provider) throws IOException {
+        if (branch == null) {
+            branch = git.getRepository().getBranch();
+        }
+
+        String result;
+        try {
+            log.info("开始重置");
+            //重置
+//            git.reset()
+//                    .setMode(ResetCommand.ResetType.HARD)
+//                    .setRef(branch).call();
+
+            log.info("开始拉取");
+            //拉取
+            git.pull()
+                    .setRemote("origin")
+                    .setRemoteBranchName("gh-pages")
+                    .call();
+            log.info("拉取成功!");
+        } catch (Exception e) {
+              e.getMessage();
+        } finally {
+            if (git != null) {
+                git.close();
+            }
+        }
+        return git;
+    }
+
+    /**
+     * 查看状态
+     */
+    public static void status() {
+        File RepoGitDir = new File("/blog/.git");
+        Repository repo = null;
+        try {
+            repo = new FileRepository(RepoGitDir.getAbsolutePath());
+            Git git = new Git(repo);
+            Status status = git.status().call();
+            log.info("Git Change: " + status.getChanged());
+            log.info("Git Modified: " + status.getModified());
+            log.info("Git UncommittedChanges: " + status.getUncommittedChanges());
+            log.info("Git Untracked: " + status.getUntracked());
+        } catch (Exception e) {
+            log.info(e.getMessage());
+        } finally {
+            if (repo != null) {
+                repo.close();
+            }
+        }
     }
 
 
