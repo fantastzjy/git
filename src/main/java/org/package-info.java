03eb70de869260b91379b0c6b3b68a1a76754b14
fantastzjy <849372293@qq.com>
2022-01-07 16:10:20 +0800

    1

---
+++ src/main/java/org/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * @Package: org
+ * @ClassName: package-info
+ * @Author: jiaying2.zhang
+ * @CreateTime: 2022-1-7 16:07
+ * @Description:
+ */
+package org;
+
+
+
+//  org文件夹下项目来自   jgit-cookbook     https://github.com/centic9/jgit-cookbook
